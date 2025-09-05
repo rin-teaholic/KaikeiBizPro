a2d1fd9bdb53e26d6a82ae4d487cad5a2a4ed91e
Rin <info@rin-m.site>
2025-09-05 19:42:25 +0900

    Update client/src/main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,41 +1,11 @@
 import { createRoot } from "react-dom/client";
-import { Router } from "wouter";
 import App from "./App";
 import "./index.css";
 
-// GitHub PagesのSPAルーティング対応
-const getLocation = () => {
-  // URLのクエリパラメータからパスを取得（404.htmlからのリダイレクト対応）
-  const urlParams = new URLSearchParams(window.location.search);
-  const pathFromQuery = urlParams.get('/');
-  
-  if (pathFromQuery) {
-    // クエリパラメータからパスを復元
-    const decodedPath = pathFromQuery.replace(/~and~/g, '&');
-    const fullPath = '/' + decodedPath;
-    
-    // URLをクリーンアップ
-    setTimeout(() => {
-      window.history.replaceState(null, '', fullPath);
-    }, 0);
-    
-    return fullPath;
-  }
-  
-  // 通常のパスルーティング
-  return window.location.pathname;
-};
+// デバッグ用のログを追加
+console.log("Current location:", window.location.href);
+console.log("Pathname:", window.location.pathname);
+console.log("Search:", window.location.search);
+console.log("Hash:", window.location.hash);
 
-const setLocation = (path: string) => {
-  // history APIを使用
-  window.history.pushState(null, '', path);
-  
-  // ルーターに通知
-  window.dispatchEvent(new PopStateEvent('popstate'));
-};
-
-createRoot(document.getElementById("root")!).render(
-  <Router hook={{ getLocation, setLocation }}>
-    <App />
-  </Router>
-);
+createRoot(document.getElementById("root")!).render(<App />);
